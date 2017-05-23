d1c777f7d930c2388fc1b900632568209fe9bd6d
luis eduardo canedo torres <lcanedo12@gmail.com>
2017-05-22 21:46:29 -0500

    listado y busqueda de las categorias

--- AppVentas/resources/views/layouts/admin.blade.php
+++ AppVentas/resources/views/layouts/admin.blade.php
@@ -87,12 +87,12 @@
             <li class="treeview">
               <a href="#">
                 <i class="fa fa-laptop"></i>
-                <span>Almac├®n</span>
+                <span>Almacen</span>
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="almacen/articulo"><i class="fa fa-circle-o"></i> Art├¡culos</a></li>
-                <li><a href="almacen/categoria"><i class="fa fa-circle-o"></i> Categor├¡as</a></li>
+                <li><a href="almacen/articulo"><i class="fa fa-circle-o"></i> Articulos</a></li>
+                <li><a href="almacen/categoria"><i class="fa fa-circle-o"></i> Categorias</a></li>
               </ul>
             </li>
             
